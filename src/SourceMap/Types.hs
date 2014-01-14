717fbac15ba3f7acef479785efdaea93e9940aa9
Chris Done <chrisdone@gmail.com>
2014-01-14 21:09:56 +0100

    Bangs for types

--- src/SourceMap/Types.hs
+++ src/SourceMap/Types.hs
@@ -11,24 +11,26 @@ import Data.Function
 
 -- | The source mapping.
 data SourceMapping = SourceMapping
-  { smFile       :: FilePath
-  , smSourceRoot :: Maybe FilePath
-  , smMappings   :: [Mapping]
+  { smFile       :: !FilePath
+  , smSourceRoot :: !(Maybe FilePath)
+  , smMappings   :: ![Mapping]
   } deriving Show
 
 -- | A mapping.
 data Mapping = Mapping
-  { mapGenerated  :: Pos
-  , mapOriginal   :: Maybe Pos
-  , mapSourceFile :: Maybe FilePath
-  , mapName       :: Maybe Text
+  { mapGenerated  :: !Pos
+  , mapOriginal   :: !(Maybe Pos)
+  , mapSourceFile :: !(Maybe FilePath)
+  , mapName       :: !(Maybe Text)
   } deriving Show
 
 -- | A source position.
 data Pos = Pos
-  { posLine   :: Int32
-  , posColumn :: Int32
+  { posLine   :: !Int32
+  , posColumn :: !Int32
   } deriving (Eq,Show)
 
+-- | Compares the line, then the column.
 instance Ord Pos where
-  compare a b = on compare posLine a b <> on compare posColumn a b
+  compare a b =
+    on compare posLine a b <> on compare posColumn a b
